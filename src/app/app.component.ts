2444b8c1a08c1d7c9d4b752ef9f3c44a219d1e58
chinmaya <ganjichinmaya01@gmail.com>
2021-06-09 07:55:32 +0530

    first-commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
 import { Component } from '@angular/core';
+import arrayWords from '../utils/words';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'words-app';
+
+  words = '';
+  limit: number = 10;
+
+  handleSlideChange(newLimit: number) {
+    this.limit = newLimit;
+  }
+  generate() {
+    this.words = arrayWords.slice(0, this.limit).join(' ');
+  }
 }
